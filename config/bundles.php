d938fdcc14c0ef8cb53285a22f2014b308b11db7
Maninder Kumar <maninder1516@gmail.com>
2024-08-25 02:16:40 +0530

    Symfony ux-turbo pack is added

--- config/bundles.php
+++ config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
 ];
